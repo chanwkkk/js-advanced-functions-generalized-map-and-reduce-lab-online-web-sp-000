3640cbd709dc0171172ba277d0763aba8598d27f
Yingqi Chen <chanwk18@hotmail.com>
2020-01-02 00:33:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ function reduce(array,func,startingPoint=0){
   return array.reduce(func,startingPoint)
 }
 
-[1, 2, true, "razmatazz"].reduce(function(a, memo){ return !!a && !!memo}))
+[1, 2, true, "razmatazz"].reduce(function(a, b){ return !!a && !!b}))
